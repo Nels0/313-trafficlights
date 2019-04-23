eba4458fdaa3c7222cb0f0d746f03e8b1c72958f
Nelson <nelson.cook98@gmail.com>
2019-04-24 11:25:47 +1200

    large refactor and organisation

--- assignment1_trafficlights/main.c
+++ assignment1_trafficlights/main.c
@@ -14,7 +14,7 @@
 
 #define PRESCALE 8
 
-//Function Declarations
+//  --FUNCTION DECLARATIONS--
 void basicLight(void);
 void basicLight2(void);
 
@@ -25,14 +25,17 @@ void speedCheck(void);
 void lightUpdate(void);
 
 
-typedef enum { //light colour aliases
+typedef enum  //light colour aliases
+{
 	GREEN = PB2,
 	YELLOW = PB1,
 	RED = PB0
 	} lightColour;
 
+// --HELPER FUNCTIONS--
 
-lightColour nextLight(lightColour lightIn){ //helper function to cycle through lights in the right order
+lightColour nextLight(lightColour lightIn) //helper function to cycle through lights in the right order
+{
 	switch(lightIn) {
 		case GREEN	:	return YELLOW;
 		case YELLOW :	return RED;
@@ -41,66 +44,37 @@ lightColour nextLight(lightColour lightIn){ //helper function to cycle through l
 	}
 }
 
+int tickToMS(int currentTick) {
+    float ratio = 256.0 * (float)PRESCALE / (float)F_CPU;
+    return (int)((float)currentTick * ratio);
+}
+
+// --GLOBAL VARIABLES--
+
+/* Variables:
+ - ----Button states: DONE
+ - Light period
+ - Bumper timestamps
+ - red light flash time/state
+ - Redlight car count for PWM
+ - last car speed for PWM
+ */
+
 //Global current light color
 lightColour currentLight = RED; //LEDs should always be updated when this is changed. Should always be accurate
 
 //Whether traffic light system is in configuration mode
 bool isConfiguring = false; // for lightUpdate
 
+//Switch states
 bool SW0 = false;
 bool LB1 = false;
 bool LB2 = false;
 bool LB3 = false;
 
+//Global timestamp (overflows every 50 years or so)
 uint32_t currentTick = 0;
 
-void buttonUpdate(void){
-	
-	//Switches are all connected to portD, respectively
-	SW0 |= (PORTD & (1<<PD0)) >> PD0;
-	
-	//LB1, 2, 3 represent SW5, 6, 7 respectively
-	LB1 |= (PORTD & (1<<PD5)) >> PD5;
-	LB2 |= (PORTD & (1<<PD6)) >> PD6;
-	LB3 |= (PORTD & (1<<PD7)) >> PD7;
-
-}
-
-int tickToMS(int currentTick) {
-    float ratio = 256.0 * (float)PRESCALE / (float)F_CPU;
-    return (int)((float)currentTick * ratio);
-}
-
-//1ms timer interrupt
-/*
-void timer() 
-{
-	time +=1;
-	
-	//Check switches
-	if a bumper switch is pressed{
-		// <1.5ms response time key
-		record timestamp
-		set output pin high as proof on oscilloscope
-	}
-	if red light switch pressed
-	{
-		set switch boolean
-	}
-	if config switch pressed 
-	{
-		set boolean
-	}
-	
-	
-		
-}
-*/
-
-ISR(TIMER0_OVF_vect){
-	buttonUpdate();
-    currentTick += 1;
-}
 
 int main(void)
 {
@@ -127,6 +101,32 @@ int main(void)
     }
 }
 
+ISR(TIMER0_OVF_vect){ //fires every overflow of timer1 (every 2.048ms)
+	buttonUpdate();
+    currentTick += 1;
+    //TODO: speed measuring
+    /* button1 pressed:
+        record timestamp
+        reset button bool
+       button2 pressed:
+        record timestamp
+        calculate speed
+        output to PWM (use PWM setter function?)
+        reset button bool
+    */
+}
+
+void buttonUpdate(void){
+    
+    //Switches are all connected to portD, respectively
+    SW0 |= (PORTD & (1<<PD0)) >> PD0;
+    
+    //LB1, 2, 3 represent SW5, 6, 7 respectively
+    LB1 |= (PORTD & (1<<PD5)) >> PD5;
+    LB2 |= (PORTD & (1<<PD6)) >> PD6;
+    LB3 |= (PORTD & (1<<PD7)) >> PD7;
+
+}
 
 void cameraCheck(void)
 {
@@ -165,69 +165,4 @@ void lightUpdate(void)
 }
 
 
-/* Variables:
- - Button states
- - Light period
- - Bumper timestamps
- - red light flash time/state
- - Redlight car count for PWM
- - last car speed for PWM
- */
-
-void basicLight(void) //Task 1 done naively
-{
-	//Initialise 3 registers as output
-	DDRB |= (1<<GREEN) | (1<<YELLOW) | (1<<RED);
-	//Set outputs to 1 (off)
-	PORTB |= (1<<GREEN) | (1<<YELLOW) | (1<<RED);
-	
-	while(1){
-		PORTB &= ~(1<<PB2);
-		_delay_ms(1000);
-		PORTB |= (1<<PB2);
-		PORTB &= ~(1<<PB1);
-		_delay_ms(1000);
-		PORTB |= (1<<PB1);
-		PORTB &= ~(1<<PB0);
-		_delay_ms(1000);
-		PORTB |= (1<<PB0);
-		
-	}
-	
-}
-
-void basicLight2(void) //Task 1 done with a timer and loops
-{
-	
-	//Initialise  registers as output
-	DDRB |= (1<<GREEN) | (1<<YELLOW) | (1<<RED);
-	//Set outputs to 1 (off)
-	PORTB |= (1<<GREEN) | (1<<YELLOW) | (1<<RED);
-	
-	
-	//Timer Setup
-	
-	uint8_t cTCCR1B = TCCR1B;	
-	//Set normal operation timer mode
-	TCCR1A = 0;
-	cTCCR1B &= ~(1<<WGM12);
-	cTCCR1B &= ~(1<<WGM13);	
-	//Set pre-scaler to 1/64
-	cTCCR1B |= (1<<CS10) | (1<<CS11);
-	cTCCR1B &= ~(1<<CS12);	
-	//Commit register settings
-	//TCCR1B should = 0000011
-	TCCR1B = cTCCR1B;
-	
-	int s_tcnt = F_CPU/64; //Number of ticks in a second
-	
-	while(1){
-		if(TCNT1 > s_tcnt){ //every second
-			PORTB |= (1<<currentLight); //Turn current light off
-			currentLight = nextLight(currentLight); //Move to next light to switch
-			PORTB &= ~(1<<currentLight); //Turn current light on
-			TCNT1 = 0; //Reset timer
-		}
-	}
-}
 
